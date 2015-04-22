6ba325cf3b7afa1afda2e5139be9ec787bde735a
bee7813993 <bee@mail.pcgame-r18.jp>
2015-04-22 16:04:12 +0900

    リクエストトップ画面の検索開始ボタンのサイズ修正

--- toolinfo.php
+++ toolinfo.php
@@ -23,6 +23,6 @@
 <input type="text" name="toolurl" class="toolinfo" size="100" value="<?php echo 'http://'.$_SERVER["SERVER_ADDR"].'/';?>" />
 </div>
 <hr />
-<a href="/request.php" > リクエストTOPに戻る </a>
+<a href="request.php" > リクエストTOPに戻る </a>
 </body>
 </html>
