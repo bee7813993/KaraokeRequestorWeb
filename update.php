36bca200b7a4cecd29369078a83434f2c0b654fb
bee7813993 <bee@mail.pcgame-r18.jp>
2016-11-08 14:31:35 +0900

    管理者用項目修正全項目対応
    管理者用の項目修正を現状すべての項目を編集できるように修正。

--- update.php
+++ update.php
@@ -1,6 +1,6 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<META http-equiv="refresh" content="1; url=requestlist_only.php">
+<META http-equiv="refresh" content="1; url=requestlist_only.php" />
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>項目修正実行</title>
 </head>
@@ -20,170 +20,79 @@ if(array_key_exists("id", $_REQUEST)) {
     printf("No ID");
     die();
 }
+$updatestring = "";
 
 if(array_key_exists("songfile", $_REQUEST)) {
     $l_songfile = $_REQUEST["songfile"];
-    
-    try{
-    $sql_u = 'UPDATE requesttable set songfile = \''. $l_songfile . '\' WHERE id = '. $l_id;
-if(!empty($DEBUG))
-print "DEBUG:".$sql_u.'<br />';
-    $ret = $db->query($sql_u);
-    }catch(PDOException $e) {
-		printf("Error: %s\n", $e->getMessage());
-		die();
-    }
+    $updatestring = $updatestring.' songfile = \''. $l_songfile . '\'';
 }
 
 if(array_key_exists("singer", $_REQUEST)) {
     $l_singer = $_REQUEST["singer"];
-
-    try{
-    $sql_u = 'UPDATE requesttable set singer = \''. $l_singer . '\' WHERE id = '. $l_id;
-if(!empty($DEBUG))
-print "DEBUG:".$sql_u.'<br />';
-    $ret = $db->query($sql_u);
-    }catch(PDOException $e) {
-		printf("Error: %s\n", $e->getMessage());
-		die();
-    }
-
+    $updatestring = $updatestring.' ,singer = \''. $l_singer . '\'';
 }
 
 if(array_key_exists("comment", $_REQUEST)) {
     $l_comment = $_REQUEST["comment"];
-
-    try{
-    $sql_u = 'UPDATE requesttable set comment = \''. $l_comment . '\' WHERE id = '. $l_id;
-if(!empty($DEBUG))
-print "DEBUG:".$sql_u.'<br />';
-    $ret = $db->query($sql_u);
-    }catch(PDOException $e) {
-		printf("Error: %s\n", $e->getMessage());
-		die();
-    }
+    $updatestring = $updatestring.' ,comment = \''. $l_comment . '\'';
 }
+
+if(array_key_exists("kind", $_REQUEST)) {
+    $l_kind = $_REQUEST["kind"];
+    $updatestring = $updatestring.' ,kind = \''. $l_kind . '\'';
+}
+
 if(array_key_exists("reqorder", $_REQUEST)) {
     $l_reqorder = $_REQUEST["reqorder"];
-
-    try{
-    $sql_u = 'UPDATE requesttable set reqorder = '. $l_reqorder . ' WHERE id = '. $l_id;
-if(!empty($DEBUG))
-print "DEBUG:".$sql_u.'<br />';
-    $ret = $db->query($sql_u);
-    }catch(PDOException $e) {
-		printf("Error: %s\n", $e->getMessage());
-		die();
-    }
+    $updatestring = $updatestring.' ,reqorder = \''. $l_reqorder . '\'';
 }
 if(array_key_exists("fullpath", $_REQUEST)) {
     $l_fullpath = $_REQUEST["fullpath"];
-
-    try{
-    $sql_u = 'UPDATE requesttable set fullpath = \''. $l_fullpath . '\' WHERE id = '. $l_id;
-if(!empty($DEBUG))
-print "DEBUG:".$sql_u.'<br />';
-    $ret = $db->query($sql_u);
-    }catch(PDOException $e) {
-		printf("Error: %s\n", $e->getMessage());
-		die();
-    }
+    $updatestring = $updatestring.' ,fullpath = \''. $l_fullpath . '\'';
 }
+
 if(array_key_exists("nowplaying", $_REQUEST)) {
     $l_nowplaying = $_REQUEST["nowplaying"];
-
-    try{
-    $sql_u = 'UPDATE requesttable set nowplaying = \''. $l_nowplaying . '\' WHERE id = '. $l_id;
-if(!empty($DEBUG))
-print "DEBUG:".$sql_u.'<br />';
-    $ret = $db->query($sql_u);
-    }catch(PDOException $e) {
-		printf("Error: %s\n", $e->getMessage());
-		die();
-    }
+    $updatestring = $updatestring.' ,nowplaying = \''. $l_nowplaying . '\'';
 }
+
 if(array_key_exists("status", $_REQUEST)) {
     $l_status = $_REQUEST["status"];
+    $updatestring = $updatestring.' ,status = \''. $l_status . '\'';
+}
 
-    try{
-    $sql_u = 'UPDATE requesttable set status = \''. $l_status . '\' WHERE id = '. $l_id;
-if(!empty($DEBUG))
-print "DEBUG:".$sql_u.'<br />';
-    $ret = $db->query($sql_u);
-    }catch(PDOException $e) {
-		printf("Error: %s\n", $e->getMessage());
-		die();
-    }
+if(array_key_exists("clientip", $_REQUEST)) {
+    $l_clientip = $_REQUEST["clientip"];
+    $updatestring = $updatestring.' ,clientip = \''. $l_clientip . '\'';
+}
 
+if(array_key_exists("clientua", $_REQUEST)) {
+    $l_clientua = $_REQUEST["clientua"];
+    $updatestring = $updatestring.' ,clientua = \''. $l_clientua . '\'';
 }
 
 if(array_key_exists("playtimes", $_REQUEST)) {
     $l_playtimes = $_REQUEST["playtimes"];
-
-    try{
-    $sql_u = 'UPDATE requesttable set playtimes = \''. $l_playtimes . '\' WHERE id = '. $l_id;
-if(!empty($DEBUG))
-print "DEBUG:".$sql_u.'<br />';
-    $ret = $db->query($sql_u);
-    }catch(PDOException $e) {
-		printf("Error: %s\n", $e->getMessage());
-		die();
-    }
-
+    $updatestring = $updatestring.' ,playtimes = \''. $l_playtimes . '\'';
 }
 
-$l_clientip = $_SERVER['REMOTE_ADDR'];
-if(array_key_exists("clientip", $_REQUEST)) {
-    $l_clientip = $_REQUEST["clientip"];
-
-    try{
-    $sql_u = 'UPDATE requesttable set clientip = \''. $l_clientip . '\' WHERE id = '. $l_id;
-if(!empty($DEBUG))
-print "DEBUG:".$sql_u.'<br />';
-    $ret = $db->query($sql_u);
-    }catch(PDOException $e) {
-		printf("Error: %s\n", $e->getMessage());
-		die();
-    }
-}
-
-$l_clientua = $_SERVER['HTTP_USER_AGENT'];
-if(array_key_exists("clientua", $_REQUEST)) {
-    $l_clientua = $_REQUEST["clientua"];
-
-    try{
-    $sql_u = 'UPDATE requesttable set clientua = \''. $l_clientua . '\' WHERE id = '. $l_id;
-if(!empty($DEBUG))
-print "DEBUG:".$sql_u.'<br />';
-    $ret = $db->query($sql_u);
-    }catch(PDOException $e) {
-		printf("Error: %s\n", $e->getMessage());
-		die();
-    }
-
-}
 
 $l_value = "";
 if(array_key_exists("secret", $_REQUEST)) {
     $l_value = $_REQUEST["secret"];
-
-    try{
-    $sql_u = 'UPDATE requesttable set secret = \''. $l_value . '\' WHERE id = '. $l_id;
-if(!empty($DEBUG))
-print "DEBUG:".$sql_u.'<br />';
-    $ret = $db->query($sql_u);
-    }catch(PDOException $e) {
-		printf("Error: %s\n", $e->getMessage());
-		die();
-    }
+    $updatestring = $updatestring.' ,secret = \''. $l_value . '\'';
 }
 
 $l_value = "";
 if(array_key_exists("loop", $_REQUEST)) {
     $l_value = $_REQUEST["loop"];
-
+    $updatestring = $updatestring.' ,loop = \''. $l_value . '\'';
+}
+print  $updatestring;
+if(strlen($updatestring) > 0){
     try{
-    $sql_u = 'UPDATE requesttable set loop = \''. $l_value . '\' WHERE id = '. $l_id;
+    $sql_u = 'UPDATE requesttable set '. $updatestring . ' WHERE id = '. $l_id;
+    print  "SQL\n<br />".$sql_u."<br />\n";
 if(!empty($DEBUG))
 print "DEBUG:".$sql_u.'<br />';
     $ret = $db->query($sql_u);
@@ -191,8 +100,8 @@ print "DEBUG:".$sql_u.'<br />';
 		printf("Error: %s\n", $e->getMessage());
 		die();
     }
-}
 
+}
 
 
 print("現在の登録状況<br>");
